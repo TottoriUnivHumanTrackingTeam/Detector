e00a413430b498cc310816595b90adc713316808
MakTak <justaway22@gmail.com>
2020-01-15 17:39:47 +0900

    ファイルのIOを非同期化

--- BeaconRepository.js
+++ BeaconRepository.js
@@ -4,57 +4,66 @@ UPDATE: 2017/12/12
 AUTHOR: MakTak
 ABOUT : ビーコンレポジトリ
 -----*/
-'use strict';
-
-const Fs = require('fs');
+"use strict";
 
 const Beacon = class Beacon {
-  constructor(detectorNumber, uuid, measuredPower, rssi, detectedTime) {
+  constructor(detectorNumber, uuid, measuredPower, rssi) {
+    const d = new Date();
     this.detectorNumber = detectorNumber;
     this.beaconID = uuid;
     this.measuredPower = measuredPower;
     this.rssi = rssi;
-    this.detectedTime = detectedTime;
+    this.detectedTime = d.getTime();
   }
-}
+
+  decode() {
+    const json = {
+      detectorNumber: this.detectorNumber,
+      beaconID: this.beaconID,
+      measuredPower: this.measuredPower,
+      rssi: this.rssi,
+      detectedTime: this.detectedTime
+    };
+
+    return json;
+  }
+};
+
+const appendFilePromise = (file, data) => {
+  return new Promise((resolve, reject) => {
+    fs.appendFile(file, data, "utf-8", error => {
+      if (error) reject(error);
+      resolve(data);
+    });
+  });
+};
 
 module.exports = class BeaconRepository {
-  static makeNewData(bleacon, detectorNumber) {
-    const d = new Date();
-    const time = d.getTime();
-    const beacon = new Beacon(detectorNumber, bleacon.uuid, bleacon.measuredPower, bleacon.rssi, time);
+  static makeData(bleacon, detectorNumber) {
+    const beacon = new Beacon(
+      detectorNumber,
+      bleacon.uuid,
+      bleacon.measuredPower,
+      bleacon.rssi
+    );
     return beacon;
   }
 
-  static logWriter(filepath, beacon) {
-    const d = new Date(beacon.detectedTime);
-    const time = String.raw`${d.getFullYear()}/${this.toDoubleDigits(d.getMonth()+1)}/${this.toDoubleDigits(d.getDate())} ${this.toDoubleDigits(d.getHours())}:${this.toDoubleDigits(d.getMinutes())}:${this.toDoubleDigits(d.getSeconds())}.${this.toTripleDigits(d.getMilliseconds())}`;
-    const log = String.raw`${beacon.detectorNumber},${beacon.beaconID},${beacon.measuredPower},${beacon.rssi},${time}`;
-    try {
-      Fs.appendFileSync(filepath+"/"+this.makeFileName(), log+"\n");
-      return log;
-    } catch(err) {
-      return false;
+  static async logWriter(filepath, dataList) {
+    for (let data of dataList) {
+      const log = String.raw`${data.detectorNumber},${data.beaconID},${data.measuredPower},${data.rssi},${data.detectedTime}`;
+      await fs.appendFilePromise(
+        filepath + "/" + this.makeFileName(),
+        log + "\n"
+      );
+      console.log(log);
     }
   }
 
-  static toDoubleDigits(num) {
-      num += "";
-      if (num.length === 1) num = "0" + num;
-      return num;
-  };
-
-  static toTripleDigits(num) {
-      num += "";
-      if (num.length === 1 ) num = "00" + num;
-      else if (num.length === 2 ) num = "0" + num;
-      return num;
-  };
-
-
   static makeFileName() {
     const d = new Date();
-    const fileName = String.raw`${d.getFullYear()}_${d.getMonth()+1}_${d.getDate()}.log`;
+    const fileName = String.raw`${d.getFullYear()}_${d.getMonth() +
+      1}_${d.getDate()}.log`;
     return fileName;
   }
 };
